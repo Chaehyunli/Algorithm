859b747c47347643b62fe9f7935b65c58211ca53
Chaehyunlim <chaehyun010104@gmail.com>
2026-05-23 21:28:16 +0900

    [level 1] Title: 콜라 문제, Time: 0.22 ms, Memory: 9.26 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
@@ -0,0 +1,13 @@
+def solution(a, b, n):
+    answer = 0
+    
+    remain = 0
+        
+    while n >= a:
+        answer += (n // a) * b
+        
+        remain = n % a
+        
+        n = (n // a) * b + remain
+    
+    return answer
