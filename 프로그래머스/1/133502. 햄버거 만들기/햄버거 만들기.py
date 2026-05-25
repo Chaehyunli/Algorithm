01b91ac8bf1774ce96443759bd72ab063ec74187
Chaehyunlim <chaehyun010104@gmail.com>
2026-05-25 17:58:08 +0900

    [level 1] Title: 햄버거 만들기, Time: 183.09 ms, Memory: 21.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
@@ -0,0 +1,17 @@
+def solution(ingredient):
+    answer = 0
+    stack = []
+    
+    for s in ingredient:
+        stack.append(s)
+        
+        if len(stack) >= 4 and stack[-4:] == [1, 2, 3, 1]:
+            
+            stack.pop()
+            stack.pop()
+            stack.pop()
+            stack.pop()
+            
+            answer += 1
+            
+    return answer
